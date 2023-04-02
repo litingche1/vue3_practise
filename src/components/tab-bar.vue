87bbb29424a2f85a9e2afe04bce7a88646a6b5fb
李廷琛 <2496272528@qq.com>
2023-04-03 00:37:15 +0800

    添加tab-bar组件

---
+++ src/components/tab-bar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="tab-bar">
+    <van-tabbar v-model="active" active-color="#ff9854">
+      <van-tabbar-item v-for="(item,index) in tabbarData" :key="item.id">
+        <span>{{ item.text }}</span>
+        <template #icon>
+          <img
+            v-if="active !== index"
+            :src="getAssetURL(item.image)"
+            alt=""
+          />
+          <img v-else :src="getAssetURL(item.imageActive)" alt="" />
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import {getAssetURL} from '../utils/load_assets'
+import tabbarData from "@/assets/data/tabbar";
+const active = ref(0);
+</script>
+
+<style lang="less" scoped>
+  
+.tab-bar {
+  // 局部定义一个变量: 只针对.tab-bar子元素才生效
+  // --van-tabbar-item-icon-size: 30px !important;
+
+  // 找到类, 对类中的CSS属性重写
+  // :deep(.class)找到子组件的类, 让子组件的类也可以生效
+  :deep(.van-tabbar-item__icon) {
+    font-size: 50px;
+  }
+
+  img {
+    height: 26px;
+  }
+}
+
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="home">
-   home
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <tabBar/>
   </div>
 </template>
 
 <script setup>
-
+import tabBar from '@/components/tab-bar.vue';
 </script>
 
 <style scoped>
