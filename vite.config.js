6060a0afdc4adc8d0f2f8b5b040f552596bab3f6
litingche1 <m18244923685@163.com>
2023-04-04 10:12:02 +0800

    变更

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+//
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins:
