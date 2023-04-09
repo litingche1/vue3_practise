cf7083220b8eafb17500b2854b1635922ad4a60b
李廷琛 <2496272528@qq.com>
2023-04-10 01:08:41 +0800

    详情页面添加页面滚动的时候切换tab

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,39 +1,42 @@
 <template>
-  <div class="detail top-page" ref="detailRef">
-    <tab-control
-      v-if="showTabControl"
-      class="tabs"
-      :titles="names"
-      @tabItemClick="tabClick"
-    />
-    <van-nav-bar
-      title="房屋详情"
-      left-text="旅途"
-      left-arrow
-      @click-left="onClickLeft"
-    />
-    <div class="main" v-if="mainPart" v-memo="[mainPart]">
-      <detail-swipe :swipe-data="mainPart.topModule.housePicture.housePics"/>
-      <detail-infos name="描述" :ref="getSectionRef" :top-infos="mainPart.topModule"/>
-      <detail-facility name="设施" :ref="getSectionRef" :house-facility="mainPart.dynamicModule.facilityModule.houseFacility"/>
-      <detail-landlord name="房东" :ref="getSectionRef" :landlord="mainPart.dynamicModule.landlordModule"/>
-      <detail-comment name="评论" :ref="getSectionRef" :comment="mainPart.dynamicModule.commentModule"/>
-      <detail-notice name="须知" :ref="getSectionRef" :order-rules="mainPart.dynamicModule.rulesModule.orderRules"/>
-      <detail-map name="周边" :ref="getSectionRef" :position="mainPart.dynamicModule.positionModule"/>
-      <detail-intro :price-intro="mainPart.introductionModule"/>
-    </div>
-    <div class="footer">
-      <img src="@/assets/img/detail/icon_ensure.png" alt="">
-      <div class="text">弘源旅途, 永无止境!</div>
-    </div>
+    <div class="detail top-page" ref="detailRef">
+        <tab-control
+                v-if="showTabControl"
+                class="tabs"
+                :titles="names"
+                @tabItemClick="tabClick"
+                ref="tabControlRef"
+        />
+        <van-nav-bar
+                title="房屋详情"
+                left-text="旅途"
+                left-arrow
+                @click-left="onClickLeft"
+        />
+        <div class="main" v-if="mainPart" v-memo="[mainPart]">
+            <detail-swipe :swipe-data="mainPart.topModule.housePicture.housePics"/>
+            <detail-infos name="描述" :ref="getSectionRef" :top-infos="mainPart.topModule"/>
+            <detail-facility name="设施" :ref="getSectionRef"
+                             :house-facility="mainPart.dynamicModule.facilityModule.houseFacility"/>
+            <detail-landlord name="房东" :ref="getSectionRef" :landlord="mainPart.dynamicModule.landlordModule"/>
+            <detail-comment name="评论" :ref="getSectionRef" :comment="mainPart.dynamicModule.commentModule"/>
+            <detail-notice name="须知" :ref="getSectionRef"
+                           :order-rules="mainPart.dynamicModule.rulesModule.orderRules"/>
+            <detail-map name="周边" :ref="getSectionRef" :position="mainPart.dynamicModule.positionModule"/>
+            <detail-intro :price-intro="mainPart.introductionModule"/>
+        </div>
+        <div class="footer">
+            <img src="@/assets/img/detail/icon_ensure.png" alt="">
+            <div class="text">弘源旅途, 永无止境!</div>
+        </div>
 
-  </div>
+    </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-import { getDetailInfos } from "@/services"
+import {ref, computed, watch} from 'vue'
+import {useRoute, useRouter} from 'vue-router'
+import {getDetailInfos} from "@/services"
 
 import TabControl from "@/components/tab-control/tab-control.vue"
 import DetailSwipe from "./cpns/detail_01-swipe.vue"
@@ -54,19 +57,19 @@ const houseId = route.params.id
 const detailInfos = ref({})
 const mainPart = computed(() => detailInfos.value.mainPart)
 getDetailInfos(houseId).then(res => {
-  detailInfos.value = res.data
+    detailInfos.value = res.data
 })
 
 // 监听返回按钮的点击
 const onClickLeft = () => {
-  router.back()
+    router.back()
 }
 
 // tabControl相关的操作
 const detailRef = ref()
-const { scrollTop } = useScroll(detailRef)
+const {scrollTop} = useScroll(detailRef)
 const showTabControl = computed(() => {
-  return scrollTop.value >= 300
+    return scrollTop.value >= 300
 })
 
 // const landlordRef = ref()
@@ -76,25 +79,41 @@ const showTabControl = computed(() => {
 // }
 const sectionEls = ref({})
 const names = computed(() => {
-  return Object.keys(sectionEls.value)
+    return Object.keys(sectionEls.value)
 })
 const getSectionRef = (value) => {
-  const name = value?.$el.getAttribute("name")
-  sectionEls.value[name] = value?.$el
+    const name = value?.$el.getAttribute("name")
+    sectionEls.value[name] = value?.$el
 }
 const tabClick = (index) => {
-  const key = Object.keys(sectionEls.value)[index]
-  const el = sectionEls.value[key]
-  let instance = el.offsetTop
-  if (index !== 0) {
-    instance = instance - 44
-  }
+    const key = Object.keys(sectionEls.value)[index]
+    const el = sectionEls.value[key]
+    let instance = el.offsetTop
+    if (index !== 0) {
+        instance = instance - 44
+    }
 
-  detailRef.value.scrollTo({
-    top: instance,
-    behavior: "smooth"
-  })
+    detailRef.value.scrollTo({
+        top: instance,
+        behavior: "smooth"
+    })
 }
+//页面滚动的时候切换tab
+const tabControlRef = ref(null)
+watch(scrollTop, newVal => {
+    const elList = Object.values(sectionEls.value)
+    console.log(elList)
+    const valList = elList.map(el => el.offsetTop)
+    console.log(valList)
+    let index = valList.length - 1
+    for (let i = 0; i < elList.length; i++) {
+        if (valList[i] > newVal + 44) {
+            index = i - 1
+            break
+        }
+    }
+    tabControlRef.value?.setCurrentIndex(index)
+})
 
 </script>
 
