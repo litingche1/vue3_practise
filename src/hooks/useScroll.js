1ee94ac69aaa7d6c5eedf37565c350c9d424cb45
litingche1 <m18244923685@163.com>
2023-04-12 10:39:49 +0800

    修复useScroll

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,4 +1,4 @@
-import {onMounted, onUnmounted, ref} from 'vue'
+import {onMounted, onUnmounted, ref, onDeactivated} from 'vue'
 import {throttle} from 'underscore'
 
 
@@ -7,29 +7,33 @@ const scrollTop = ref(0)
 const scrollHeight = ref(0)
 const isReachBottom = ref(false)
 export default function useScroll(elRef) {
-    let el = window
-    const callBack = throttle(() => {
-        if (el === window) {
-            clientHeight.value = document.documentElement.clientHeight
-            scrollTop.value = document.documentElement.scrollTop
-            scrollHeight.value = document.documentElement.scrollHeight
-        } else {
-            clientHeight.value = el.clientHeight
-            scrollTop.value = el.scrollTop
-            scrollHeight.value = el.scrollHeight
-        }
+  let el = window
+  const callBack = throttle(() => {
+    if (el === window) {
+      clientHeight.value = document.documentElement.clientHeight
+      scrollTop.value = document.documentElement.scrollTop
+      scrollHeight.value = document.documentElement.scrollHeight
+    } else {
+      clientHeight.value = el.clientHeight
+      scrollTop.value = el.scrollTop
+      scrollHeight.value = el.scrollHeight
+    }
 
-        if (clientHeight.value * 1 + scrollTop.value * 1 >= scrollHeight.value) {
-            isReachBottom.value = true
-        }
-    }, 100)
-    onMounted(() => {
-        if (elRef) el = elRef.value
-        el.addEventListener('scroll', callBack)
-    })
-    onUnmounted(() => {
-        el.removeEventListener('scroll', callBack)
-        scrollTop.value=0
-    })
-    return {clientHeight, scrollTop, scrollHeight, isReachBottom}
+    if (clientHeight.value * 1 + scrollTop.value * 1 >= scrollHeight.value) {
+      isReachBottom.value = true
+    }
+  }, 100)
+  onMounted(() => {
+    if (elRef) el = elRef.value
+    el.addEventListener('scroll', callBack)
+  })
+  onUnmounted(() => {
+    el.removeEventListener('scroll', callBack)
+    scrollTop.value = 0
+  })
+  onDeactivated(() => {
+    el.removeEventListener('scroll', callBack)
+    scrollTop.value = 0
+  })
+  return {clientHeight, scrollTop, scrollHeight, isReachBottom}
 }
